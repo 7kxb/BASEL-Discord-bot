eb715102216ab1a1925e09bbec8ec33d6fc3b0c3
linux <pop@pop-os.localdomain>
2023-03-04 12:55:45 -0500

    idk

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const tree = new SlashCommandBuilder()
   .setName('menu')
   .setDescription('Menu that integrates with the BASEL API.')
   .setDefaultPermission(true);
-const getAll = new SlashCommandBuilder()
-  .setName('getall')
-  .setDescription('Test command')
-  .setDefaultPermission(true);
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -22,81 +18,104 @@ client.once('ready', () => {
       .create(tree)
       .then((command) => console.log(`Created command ${command.name}`))
       .catch(console.error);
-    guild.commands
-      .create(getAll)
-      .then((command) => console.log(`Created command ${command.name}`))
-      .catch(console.error);
   });
 });
 
+async function mainMenu(interaction) {
+  const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('http://172.105.103.136/')
+    .setURL('http://172.105.103.136/')
+    .setAuthor({ name: 'BASEL', iconURL: 'https://cdn.discordapp.com/attachments/1074487980045115393/1081575553724854412/logo-color.png', url: 'http://172.105.103.136/' })
+    .setDescription('Menu that integrates with the BASEL API.')
+    .setThumbnail('https://cdn.discordapp.com/attachments/1074487980045115393/1081575553724854412/logo-color.png')
+    .addFields(
+      { name: 'Navigate using the select menu.', value: 'getAll, searchKeyword, fromUser' },
+    )
+    .setTimestamp()
+    .setFooter({ text: 'Reduce Reuse Recycle', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+  const row = new ActionRowBuilder()
+    .addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId('select')
+        .setPlaceholder('Nothing selected')
+        .addOptions(
+          {
+            label: 'getAll',
+            description: 'Gets all items in a catagory',
+            value: 'first_option',
+          },
+          {
+            label: 'searchKeyword',
+            description: 'Gets all items containing a keyword',
+            value: 'second_option',
+          },
+          {
+            label: 'fromUser',
+            description: 'Gets all items from a user',
+            value: 'third_option',
+          },
+        ),
+    );
+  await interaction.reply({ embeds: [exampleEmbed], components: [row], ephemeral: true });
+}
+
+async function getAll(interaction) {
+  const url = 'http://172.105.103.136/api/products/getAll/';
+  const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('primary')
+          .setLabel('Back')
+          .setStyle(ButtonStyle.Primary),
+      );
+  try {
+    const response = await axios.get(url);
+    const json_data = response.data;
+    let response_str = '```';
+    for (let data of json_data) {
+      response_str += `
+      ID: ${data.id}
+      Name: ${data.name}
+      Description: ${data.description}
+      Image: ${data.image}
+      Seller: ${data.seller}
+      Price: $${data.price}
+      `;
+    }
+    response_str += '```';
+    interaction.reply({ content: response_str, components: [row], ephemeral: true });
+  } catch (error) {
+    console.error(error);
+    interaction.reply({ content: 'Error', components: [row], ephemeral: true });
+  }
+}
+
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
-  if (interaction.commandName === 'getall') {
-    const url = 'http://172.105.103.136/api/products/getAll/';
-    try {
-      const response = await axios.get(url);
-      const json_data = response.data;
-      let response_str = '```';
-      for (let data of json_data) {
-        response_str += `
-        ID: ${data.id}
-        Name: ${data.name}
-        Description: ${data.description}
-        Image: ${data.image}
-        Seller: ${data.seller}
-        Price: $${data.price}
-        `;
-      }
-      response_str += '```';
-      await interaction.reply({ content: response_str, ephemeral: true });
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'Error', ephemeral: true });
-    }
-  }
   if (interaction.commandName === 'menu') {
-    const exampleEmbed = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setTitle('http://172.105.103.136/')
-      .setURL('http://172.105.103.136/')
-      .setAuthor({ name: 'BASEL', iconURL: 'https://cdn.discordapp.com/attachments/1074487980045115393/1081575553724854412/logo-color.png', url: 'http://172.105.103.136/' })
-      .setDescription('Menu that integrates with the BASEL API.')
-      .setThumbnail('https://cdn.discordapp.com/attachments/1074487980045115393/1081575553724854412/logo-color.png')
-      .addFields(
-        { name: 'Navigate using the select menu.', value: 'getAll, searchPublic, fromUser' },
-      )
-      .setTimestamp()
-      .setFooter({ text: 'Reduce Reuse Recycle', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-    const row = new ActionRowBuilder()
-			.addComponents(
-				new StringSelectMenuBuilder()
-					.setCustomId('select')
-					.setPlaceholder('Nothing selected')
-					.addOptions(
-						{
-							label: 'getAll',
-							description: 'Gets all items in a catagory',
-							value: 'first_option',
-						},
-						{
-							label: 'searchPublic',
-							description: 'Gets all items containing a keyword',
-							value: 'second_option',
-						},
-            {
-							label: 'fromUser',
-							description: 'Gets all items from a user',
-							value: 'third_option',
-						},
-					),
-			);
-    interaction.reply({ embeds: [exampleEmbed], components: [row], ephemeral: true });
+    mainMenu(interaction);
   }
 });
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isButton()) return;
-  interaction.reply({ content: 'clicked', ephemeral: true });
+  mainMenu(interaction);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isStringSelectMenu()) return;
+  const selected = interaction.values[0];
+  console.log(selected);
+	if (selected === 'first_option') {
+		getAll(interaction);
+	}
+  if (selected === 'second_option') {
+		searchKeyword(interaction);
+	}
+  if (selected === 'third_option') {
+		fromUser(interaction);
+	}
 });
 
 fs.readFile('token.txt', 'utf8', function (err, data) {
