b712b8a2159ffb37385b3174ec1914c246b0de85
linux <pop@pop-os.localdomain>
2023-03-04 01:36:04 -0500

    3

--- main.py
+++ main.py
@@ -15,20 +15,25 @@ async def on_message(message):
         return
     elif message.content.startswith("getAll"):
         url = "http://172.105.103.136/api/products/getAll/"
-        response = requests.get(url)
         args = message.content.split()
         if len(args) > 1:
             url += args[1]
+            print(args[1])
+            print(url)
+        response = requests.get(url)
         if response.status_code == 200:
             json_data = response.json()
-            await message.reply("```\n" +
-                                "URL: " + url + "\n"
-                                "Name: " + json_data[0]["name"] + "\n" +
-                                "Description: " + json_data[0]["description"] + "\n" +
-                                "Image: " + json_data[0]["image"] + "\n" +
-                                "Seller: " + json_data[0]["seller"] + "\n" +
-                                "Price: " + str(json_data[0]["price"]) + "\n" +
-                                "```")
+            for i in range(len(json_data[0])):
+                await message.reply("``\n" +
+                                    "URL: " + url + "\n" +
+                                    "ID: " + str(json_data[0][i]["id"]) + "\n" +
+                                    "Name: " + json_data[0][i]["name"] + "\n" +
+                                    "Description: " + json_data[0][i]["description"] + "\n" +
+                                    "Image: " + json_data[0][i]["image"] + "\n" +
+                                    "Seller: " + json_data[0][i]["seller"] + "\n" +
+                                    "Price: $" + str(json_data[0][i]["price"]) + "\n" +
+                                    "``")
+                await message.reply(json_data)
         else:
             await message.reply("error")
 
