3183f64c767cf33895f1de87636e8402c8422fe8
linux <pop@pop-os.localdomain>
2023-03-04 01:00:59 -0500

    api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import requests
 
 TOKEN = ''
 
@@ -12,7 +13,23 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    elif "ping" in message.content:
-        await message.channel.send("pong")
+    elif message.content.startswith("getAll"):
+        url = "http://172.105.103.136/api/products/getAll/"
+        response = requests.get(url)
+        args = message.content.split()
+        if len(args) > 1:
+            url += args[1]
+        if response.status_code == 200:
+            json_data = response.json()
+            await message.reply("```\n" +
+                                "URL: " + url + "\n"
+                                "Name: " + json_data[0]["name"] + "\n" +
+                                "Description: " + json_data[0]["description"] + "\n" +
+                                "Image: " + json_data[0]["image"] + "\n" +
+                                "Seller: " + json_data[0]["seller"] + "\n" +
+                                "Price: " + str(json_data[0]["price"]) + "\n" +
+                                "```")
+        else:
+            await message.reply("error")
 
 client.run(TOKEN)
